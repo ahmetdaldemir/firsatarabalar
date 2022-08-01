a1b736c3adedbbd998a9c58e558041b2cd559386
ahmet.daldemir <ahmetdaldemir@gmail.com>
2022-08-01 09:17:17 +0300

    Updated

---
+++ app/Http/Controllers/Api/VehicleRequest.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class VehicleRequest extends Controller
+{
+    private VehicleRepositoryInterface $VehicleRepository;
+
+    public function __construct(VehicleRepositoryInterface $VehicleRepository)
+    {
+        $this->VehicleRepository = $VehicleRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $data = $this->VehicleRepository->get();
+        return \response()->json([
+            'data' => $data,
+        ], 200);
+    }
+}
